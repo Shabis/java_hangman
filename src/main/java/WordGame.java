dd47c74b77288f3b78608bc687b1ab1ebdf187e5
Shelby Clayton and Sara Jensen <sara@>
2016-09-08 11:32:41 -0700

    first test passes

---
+++ src/main/java/WordGame.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class WordGame {
+
+  public boolean printsWord(String play) {
+    List<String> myWordList = new ArrayList<String>();
+      myWordList.add("instantiation");
+      myWordList.add("programming");
+      myWordList.add("encapsulation");
+      myWordList.add("visibility");
+
+    for ( String word : myWordList ) {
+      // return true;
+    }
+    return false;
+  }
+
+
+}
---
+++ src/test/java/WordGameTest.java
@@ -0,0 +1,13 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class WordGameTest {
+
+  @Test
+  public void printsWord_printsWord_String(){
+    WordGame testWordGame = new WordGame();
+    assertEquals(true, testWordGame.printsWord("play"));
+  }
+
+
+}
